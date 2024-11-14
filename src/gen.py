25d5c6f6f0f8a1d07b842c498cd2eb8424753c08
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-11-14 13:39:40 -0500

    fixed data loading

--- src/gen.py
+++ src/gen.py
@@ -7,16 +7,21 @@ def makeData(num_samples = 1, config = "random", train = True):
     for _ in range(num_samples):
         cube = getToDesired(config = config)
         tmp_cube = cube.copy()
-        cube = np.array(cube)
         solve_moves = solveNow(tmp_cube)
         data.append(cube)
         move_data.append(solve_moves)
-    data = np.array(data)
-    move_data = np.array(move_data)
     dir = "train" if train else "test"
-    np.save(f'../data/{dir}/cube.npy', data)
-    np.save(f'../data/{dir}/cube_moves.npy', move_data)
+    np.savez(f'../data/{dir}/cube', *data)
+    np.savez(f'../data/{dir}/cube_moves', *move_data)
 
 def loadData(train = True):
     dir = "train" if train else "test"
-    return (np.load(f'../data/{dir}/cube.npy'), np.load(f'../data/{dir}/cube_moves.npy'))
+    ret1 = []
+    ret2 = []
+    cube = np.load(f"../data/{dir}/cube.npz")
+    cube_moves = np.load(f"../data/{dir}/cube_moves.npz")
+    for k, v in cube.items():
+        ret1.append(v.tolist())
+    for k, v in cube_moves.items():
+        ret2.append(v.tolist())
+    return (ret1, ret2)
