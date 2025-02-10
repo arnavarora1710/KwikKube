369dcafe5db6874b855d7533ca3d91ed32027cdc
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-02-10 03:11:20 -0500

    bluh

--- src/matrix_repr.py
+++ src/matrix_repr.py
@@ -3,12 +3,25 @@ import numpy as np
 class Cube:
     def __init__(self):
         self.cube = np.eye(24, dtype=bool)
+        self.ref = np.eye(24, dtype=bool)
+
+    def isValid(self):
+        pass
+
+    def isSolved(self):
+        return self.cube == self.ref
 
     def move(self, move):
+        # figure out what sticker moved where
+        # example: if U is the move, then
+        # then the stickers on the top face move counterclockwise
+        # so look at the first 8 columns of the matrix
+        # find the 1s in these columns
+        # cycle them clockwise
         if move == "U":
-            pass
+            self.cube[:, :8] = np.roll(self.cube[:, :8], -1, axis=1)
         elif move == "Ui":
-            pass
+            self.cube[:, :8] = np.roll(self.cube[:, :8], 1, axis=1)
         elif move == "R":
             pass
         elif move == "Ri":
