77dc2d935538b27559b83dd16a5a3277c79ede56
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-03-02 00:57:37 -0500

    added matrix transforms for each move

--- src/matrix_repr.py
+++ src/matrix_repr.py
@@ -4,6 +4,15 @@ class Cube:
     def __init__(self):
         self.cube = np.eye(24, dtype=bool)
         self.ref = np.eye(24, dtype=bool)
+        # self.mapping = {}
+        # # mapping[i] stores j such that cube[j][i] = 1 for cubelet i
+        # # meaning that (original) cubelet i is at cubelet j
+        # for i in range(24):
+        #     self.mapping[i] = i
+
+    def isValidMove(self):
+        # determinant of cube must not be 0
+        return np.linalg.det(self.cube) != 0
 
     def isValid(self):
         pass
@@ -19,29 +28,33 @@ class Cube:
         # find the 1s in these columns
         # cycle them clockwise
         if move == "U":
-            self.cube[:, :8] = np.roll(self.cube[:, :8], -1, axis=1)
+            self.cube[:, :8] = np.roll(self.cube[:, :8], -2, axis=1)
         elif move == "Ui":
-            self.cube[:, :8] = np.roll(self.cube[:, :8], 1, axis=1)
+            self.cube[:, :8] = np.roll(self.cube[:, :8], 2, axis=1)
         elif move == "R":
-            pass
+            # want to roll (2, 3, 4, 12, 20, 19, 18, 10)
+            self.cube[:, [2, 3, 4, 12, 20, 19, 18, 10]] = np.roll(self.cube[:, [2, 3, 4, 12, 20, 19, 18, 10]], 2, axis=1)
         elif move == "Ri":
-            pass
+            self.cube[:, [2, 3, 4, 12, 20, 19, 18, 10]] = np.roll(self.cube[:, [2, 3, 4, 12, 20, 19, 18, 10]], -2, axis=1)
         elif move == "F":
-            pass
+            # want to roll (0, 1, 2, 10, 18, 17, 16, 8)
+            self.cube[:, [0, 1, 2, 10, 18, 17, 16, 8]] = np.roll(self.cube[:, [0, 1, 2, 10, 18, 17, 16, 8]], 2, axis=1)
         elif move == "Fi":
-            pass
+            self.cube[:, [0, 1, 2, 10, 18, 17, 16, 8]] = np.roll(self.cube[:, [0, 1, 2, 10, 18, 17, 16, 8]], -2, axis=1)
         elif move == "D":
-            pass
+            self.cube[:, 16:] = np.roll(self.cube[:, 16:], 2, axis=1)
         elif move == "Di":
-            pass
+            self.cube[:, 16:] = np.roll(self.cube[:, 16:], -2, axis=1)
         elif move == "L":
-            pass
+            # want to roll (0, 7, 6, 14, 22, 21, 20, 8)
+            self.cube[:, [0, 7, 6, 14, 22, 21, 20, 8]] = np.roll(self.cube[:, [0, 7, 6, 14, 22, 21, 20, 8]], -2, axis=1)
         elif move == "Li":
-            pass
+            self.cube[:, [0, 7, 6, 14, 22, 21, 20, 8]] = np.roll(self.cube[:, [0, 7, 6, 14, 22, 21, 20, 8]], 2, axis=1)
         elif move == "B":
-            pass
+            # want to roll (4, 5, 6, 14, 22, 21, 20, 12)
+            self.cube[:, [4, 5, 6, 14, 22, 21, 20, 12]] = np.roll(self.cube[:, [4, 5, 6, 14, 22, 21, 20, 12]], 2, axis=1)
         elif move == "Bi":
-            pass
+            self.cube[:, [4, 5, 6, 14, 22, 21, 20, 12]] = np.roll(self.cube[:, [4, 5, 6, 14, 22, 21, 20, 12]], -2, axis=1)
         else:
             raise ValueError(f"Invalid move: {move}")
 
